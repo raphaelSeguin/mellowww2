9f3ef9bc7e201d09a6268c42f71e4ba4bbbcda6f
raphaelSeguin <raphaelseguin@gmail.com>
2018-09-24 14:35:43 +0200

    fix back

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,19 +16,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// snd
+app.use('/snd', express.static(path.join(__dirname, '../client/assets/snd')));
 
 if (process.env.NODE_ENV === 'production') {
+    // assets
     app.use('/', express.static(path.join(__dirname, '../client/build')));
-    app.use('/snd', express.static(path.join(__dirname, '../client/assets/snd')));
-
+    
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    })
 }
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
