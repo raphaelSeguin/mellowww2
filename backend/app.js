9612b77acfe550226844656158ebcffc66abc8b2
raphaelSeguin <raphaelseguin@gmail.com>
2018-11-28 21:02:31 +0100

    mmm ok

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // snd
-app.use('/snd', express.static(path.join(__dirname, '../client/assets/snd')));
+app.use('/snd', express.static(path.join(__dirname, '../client/public/snd')));
 
 if (process.env.NODE_ENV === 'production') {
     // assets
