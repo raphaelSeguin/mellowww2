fd8912d921d83593486859884d2fc1c22ba0262a
raphaelSeguin <raphaelseguin@gmail.com>
2018-12-04 12:55:36 +0100

    touch events and audioFactory

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,35 @@ import './App.css';
 import Controls from './Controls.js';
 import Keyboard from './Keyboard.js';
 
-import audio from './audio.js';
+import audioFactory from './audio.js';
+import {btnMaxDist, sliderWidth} from './constants.js';
 
-import {btnMaxDist, sliderWidth} from './constants.js'
+const audio = audioFactory();
+
+// helpers
+// diff between old and new array
+const addedItems = (oldArray, newArray) => 
+    newArray.reduce( (result, item, i) => 
+            oldArray.includes(item) ? result : result.concat(item), []);
+
+const removedItems = (oldArray, newArray) => addedItems(newArray, oldArray);
+
+const removeDoubles = array => 
+    array.reduce( (result, item, i, array) => 
+        array.slice(i+1).includes(item) ? result : result.concat(item), []
+    )
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             bank: '',
-            instrument: 'flute',
+            instrument: 'Flute', // attention si aucun instrument n'est selectionné au départ, ça crashe !
             volume: 0.8,
             tone: 0.5,
             pitch: 0.5,
             cursor: 0.1,
+            prevKeysPressed: [],
             keysPressed: [],
             lastKeyPressed: null,
             selectParam: '',
@@ -35,29 +50,40 @@ class App extends Component {
         })
     }
     handleKeyboard(keyNumber, type) {
-        const releaseKey = key => {
-            this.setState({
-                keysPressed: this.state.keysPressed.filter( n => n !== key)
-            })
-        }
-        if (type === 'mousedown') {
-            this.setState({
-                keysPressed: this.state.keysPressed.concat(keyNumber),
-                lastKeyPressed: keyNumber
-            })
+        console.log('key : ', keyNumber, '|| type : ', type);
+        const pushKey = key =>
+            this.setState( 
+                state => ({
+                    keysPressed: state.keysPressed.concat(key)
+                })
+            )
+        const releaseKey = key =>
+            this.setState(
+                state => ({
+                    keysPressed: state.keysPressed.filter( n => n !== key)
+                })
+            )
+        // switch à la place ? 
+        if (type === 'mousedown' || type === 'touchstart') {
+            this.setState(
+                state => ({
+                    keysPressed: state.keysPressed.concat(keyNumber),
+                    lastKeyPressed: keyNumber
+                })
+            )
             //
             // JUSTE POUR VOIR  => => => utiliser this.state pour déclencher les notes 
             // à partir de componentDidUpdate (... ou pas si c'est trop lent...)
-            console.log('noteOn');
+            //console.log('noteOn');
             audio.noteOn(keyNumber);
             //
             //
         }
-        else if (type === 'mouseleave' || type === 'mouseout' || type === 'mouseup') {
+        else if (type === 'mouseleave' || type === 'mouseout' || type === 'mouseup' || type === 'touchend') {
             releaseKey(this.state.lastKeyPressed);
             //
             //
-            console.log('noteOff');
+            //console.log('noteOff');
             audio.noteOff(keyNumber);
             //
             //
@@ -65,6 +91,7 @@ class App extends Component {
     }
     clickHandler(e) {
         //e.persist();
+        e.preventDefault();
         const target = e.target.id;
         const type = e.type;
         if (target.slice(0, 3) === 'key') {
@@ -132,16 +159,16 @@ class App extends Component {
                 onMouseDown={this.clickHandler}
                 onMouseMove={this.clickHandler}
                 onMouseUp={this.clickHandler} 
-                onMouseLeave={this.clickHandler}>
+                onMouseLeave={this.clickHandler} 
+                onTouchCancel={this.clickHandler} 
+                onTouchEnd={this.clickHandler} 
+                onTouchMove={this.clickHandler} 
+                onTouchStart={this.clickHandler} >
                 <Controls
                     handler={this.changeParam}
                     state={this.state} />
                 <Keyboard
-                    state={this.state} 
-                    onMouseDown={this.clickHandler}
-                    onMouseMove={this.clickHandler}
-                    onMouseUp={this.clickHandler}
-                    onMouseLeave={this.clickHandler}
+                    state={this.state}
                     handler={this.clickHandler}/>
             </div>
         );
--- client/src/audio.js
+++ client/src/audio.js
@@ -4,84 +4,99 @@ import SamplePlayer from './SamplePlayer.js'
 // helper
 const map = (val, min, max, tmin, tmax) => ((val - min) / (max - min)) * (tmax - tmin) + tmin;
 
-const audio = {}
-
-audio.sourceNodes = {};
-
-audio.ctx = new (window.AudioContext || window.webkitAudioContext)();
-
-// registre des bufferSources actifs
-audio.keyboard = [];
-audio.settings = {
-    volume:0.8,
-    tone: 50, //
-    pitch: 0. // attention sur l'interface 0.5 équivaut à 0...
-}
-
-audio.resume = function() {
-    this.ctx.resume();
-}
-
-// notes
-audio.noteOn = function(note) {
-    this.keyboard[note] = new SamplePlayer(this.ctx,
-        this.sourceNodes[this.instrument][note],
-        this.settings);
-    this.keyboard[note].on();
-}
-audio.noteOff = function(note) {
-    if (this.keyboard[note]) {
-        this.keyboard[note].off();
+// 
+const audioFactory = () => {
+    //const audio = {};
+    const sourceNodes = {};
+    const ctx = new (window.AudioContext || window.webkitAudioContext)();
+    // registre des bufferSources actifs
+    const keyboard = [];
+    const settings = {
+        volume:0.8,
+        tone: 50,
+        pitch: 0.
     }
-}
+    const resume = function resume() {
+        this.ctx.resume();
+    }
 
-// preset
-audio.setInstrument = function(instrument) {
-    this.instrument = instrument;
-}
-
-// audio params
-audio.setPitch = function(semi) {
-    const pitch = map(semi, 0, 1, -2, 2);
-    this.settings.pitch = pitch;
-    this.keyboard.forEach( node => node.setPitch(pitch)  )
-}
-audio.setVolume = function(volume) {
-    this.settings.volume = volume;
-    this.keyboard.forEach( node => { node.setVolume(volume)} )
-}
-audio.setTone = function(tone) {
-    const frequency = map(tone, 0, 1, 50, 1000);
-    this.settings.tone = frequency;
-    this.keyboard.forEach( node => node.setFilter(frequency));
-}
-
-// populate sourceNodes with samples
-audio.loadSamples = function() {
-    for (let instrument in samples) {
-        this.sourceNodes[instrument] = [];
-        for (let i = 0; i < samples[instrument].length; i++) {
-            const url = '/snd/' + instrument + '/' + encodeURIComponent(samples[instrument][i]);
-            this.loadSample(url, instrument, i );
+    // notes
+    const noteOn = function noteOn(note) {
+        this.keyboard[note] = new SamplePlayer(this.ctx,
+            this.sourceNodes[this.instrument][note],
+            this.settings);
+        this.keyboard[note].on();
+    }
+    const noteOff = function noteOff(note) {
+        if (this.keyboard[note]) {
+            this.keyboard[note].off();
         }
     }
-}
 
-audio.loadSample = function(url, instrument, number) {
-    const request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = () => {
-        this.ctx.decodeAudioData(
-            request.response,
-            decodedSample => {
-                // AudioBuffer
-                this.sourceNodes[instrument][number] = decodedSample;
-            },
-            error => console.log('ERROR LOADING SAMPLE : ' + error)
-        );
+    // preset
+    const setInstrument = function setInstrument(instrument) {
+        this.instrument = instrument;
     }
-    request.send();
+
+    // audio params
+    const setPitch = function setPitch(semi) {
+        const pitch = map(semi, 0, 1, -2, 2);
+        this.settings.pitch = pitch;
+        this.keyboard.forEach( node => node.setPitch(pitch)  )
+    }
+    const setVolume = function setVolume(volume) {
+        this.settings.volume = volume;
+        this.keyboard.forEach( node => { node.setVolume(volume)} )
+    }
+    const setTone = function setTone(tone) {
+        const frequency = map(tone, 0, 1, 50, 1000);
+        this.settings.tone = frequency;
+        this.keyboard.forEach( node => node.setFilter(frequency));
+    }
+
+    // populate sourceNodes with samples
+    const loadSamples = function loadSamples() {
+        for (let instrument in samples) {
+            this.sourceNodes[instrument] = [];
+            for (let i = 0; i < samples[instrument].length; i++) {
+                const url = '/snd/' + instrument + '/' + encodeURIComponent(samples[instrument][i]);
+                this.loadSample(url, instrument, i );
+            }
+        }
+    }
+
+    const loadSample = function loadSample(url, instrument, number) {
+        const request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = 'arraybuffer';
+        request.onload = () => {
+            this.ctx.decodeAudioData(
+                request.response,
+                decodedSample => {
+                    // AudioBuffer
+                    this.sourceNodes[instrument][number] = decodedSample;
+                },
+                error => console.log('ERROR LOADING SAMPLE : ' + error)
+            );
+        }
+        request.send();
+    }
+
+    return {
+        sourceNodes,
+        ctx,
+        keyboard,
+        settings,
+        resume,
+        noteOn,
+        noteOff,
+        setInstrument,
+        setPitch,
+        setVolume,
+        setTone,
+        loadSamples,
+        loadSample
+    }
 }
 
-export default audio;
+export default audioFactory;
